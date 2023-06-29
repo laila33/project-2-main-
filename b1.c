19bd8161601514b71cda463790e7bf727049fdaf
tarneem <tarneemahmed2002@gmail.com>
2023-06-29 13:40:52 -0700

    add

--- b1.c
+++ b1.c
@@ -42,82 +42,113 @@ int unsetalias_fun(info_tt *info, char *s)
 /**
  * setalias_fun - set alias to string
  *
- * @info: struct
- * @s: string
+ * @v_name: struct
+ * @lue: string
  *
  * Return: 0 success 1 if fail
 */
 
-int setalias_fun(info_tt *info, char *s)
+int setalias_fun(char *v_name, char *lue)
 {
-	char *p;
+	alias_t *temp = aliases;
+	int l, n, x;
+	char *new_lue;
 
-	p = strchr_func2(s, '=');
-	if (!p)
-		return (1);
-	if (!*++p)
-		return (unsetalias_fun(info, s));
-
-	unsetalias_fun(info, s);
-	return (insert_node(&(info->alias), s, 0) == NULL);
+	*lue = '\0';
+	lue++;
+	l = strlen_func(lue) - _strspn(lue, "'\"");
+	new_lue = malloc(sizeof(char) * (l + 1));
+	if (!new_lue)
+		return;
+	for (n = 0, x = 0; lue[n]; n++)
+	{
+		if (lue[n] != '\'' && lue[n] != '"')
+			new_lue[x++] = lue[n];
+	}
+	new_lue[x] = '\0';
+	while (temp)
+	{
+		if (strcmp_func(v_name, temp->name) == 0)
+		{
+			free(temp->lue);
+			temp->lue = new_lue;
+			break;
+		}
+		temp = temp->next;
+	}
+	if (!temp)
+		add_alias_end(&aliases, v_name, new_lue);
 }
 
 /**
  * printalias_fun - print alias string
  *
- * @node: alias node
+ * @alias: alias node
  *
  * Return: 0 success 1 if fail
 */
 
-int printalias_fun(my_list_t *node)
+int printalias_fun(alias_t *alias)
 {
-	char *p = NULL, *l = NULL;
+	char *aliasstg;
+	int l = strlen_func(alias->name) + strlen_func(alias->lue) + 4;
 
-	if (node)
-	{
-		p = strchr_func2(node->s, '=');
-		for (l = node->s; l <= p; l++)
-			putchar(*l);
-		putchar('\'');
-		puts_func(p + 1);
-		puts_func("'\n");
-		return (0);
-	}
-	return (1);
+	aliassg = malloc(sizeof(char) * (l + 1));
+	if (!aliassg)
+		return;
+	strcpy_func1(aliasstg, alias->name);
+	strcat_func(aliassg, "='");
+	strcat_func(aliassg, alias->lue);
+	strcat_func(aliassg, "'\n");
+
+	write(STDOUT_FILENO, aliassg, l);
+	free(aliassg);
 }
 
 /**
  * alias_fun - man alias
  *
- * @info: struct
+ * @ar: struct
+ * @fr: struct
  *
  * Return: 0
 */
 
-int alias_fun(info_tt *info)
+int alias_fun(char **ar, char **fr)
 {
-	int n = 0;
-	char *p = NULL;
-	my_list_t *node = NULL;
+	alias_t *temp = aliases;
+	int n, rt = 0;
+	char *lue;
 
-	if (info->arg_c == 1)
+	if (!ar[0])
 	{
-		node = info->alias;
-		while (node)
+		while (temp)
 		{
-			printalias_fun(node);
-			node = node->next;
+			printalias_fun(temp);
+			temp = temp->next;
 		}
-		return (0);
+		return (rt);
 	}
-	for (n = 1; info->arg_v[n]; n++)
+	for (n = 0; ar[n]; n++)
 	{
-		p = strchr_func2(info->arg_v[n], '=');
-		if (p)
-			setalias_fun(info, info->arg_v[n]);
+		temp = aliases;
+		lue = strchr_func2(ar[n], '=');
+		if (!lue)
+		{
+			while (temp)
+			{
+				if (strcmp_func(ar[n], temp->name) == 0)
+				{
+					printalias_fun(temp);
+					break;
+				}
+				temp = temp->next;
+			}
+			if (!temp)
+				rt = create_error(ar + n, 1);
+		}
 		else
-			printalias_fun(start_node(info->alias, info->arg_v[n], '='));
+			setalias_fun(ar[n], lue);
 	}
-	return (0);
+	return (rt);
 }
